78fa40fec0fd3be2af7a1ccf3418bcf9369f5700
Cole Roberts <coleroberts76@gmail.com>
2023-02-14 21:38:31 -0600

    added starter code and a service.js file

---
+++ note-taker-2/note-taker/develop/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const inquirer = require('inquirer');
+
+const port = 3001;
+
+app.listen(PORT, () => {
+    console.log('listening on PORT ${PORT}')
+});
+
+inquirer.prompt ([
+    {
+        type:
+        name:
+        message:
+
+    }
+])
+
