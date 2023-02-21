f401f532744667a56b356e8ca7b1e4f7ff8e3497
Cole Roberts <coleroberts76@gmail.com>
2023-02-20 18:39:40 -0600

    Got the app to save user notes and write it to db.json

--- routes/notesRoute.js
+++ routes/notesRoute.js
@@ -1,24 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
+const path = require('path');
 const uuid = require('uuid');
 const notesData = require('../db/db.json');
 
 router.get('/', (req, res) => res.json(notesData));
 
 router.post('/', (req, res) => {
-    const newNote = req.body;
-        if (newNote === 0) {
-            res.status(400).json;
-            console.log('newNote needs a title and text!');
-        } else {
+    const newNoteTitle = req.body.title;
+    const newNoteText = req.body.text;
+
+        if (newNoteTitle !== null && newNoteTitle !== undefined && newNoteText !== null && newNoteText !== undefined) {
             const newNoteData = {
-                title,
-                text,
-                note_id: uuid.v4(),
+                title: newNoteTitle,
+                text: newNoteText,
+                note_id: uuid.v4()
             };
             notesData.push(newNoteData);
-            res.json(notesData);
-        }
-});
+            
+            const noteDbPath = path.join(__dirname,'../db/db.json');
+            const noteDbData = JSON.stringify(notesData);
+
+            fs.writeFile(noteDbPath, noteDbData, (err) => {
+            if (err) {
+                res.status(500).json({msg: 'Error writing to database!'});
+                console.log(err);
+            } else {
+                res.json(newNoteData)
+            }
+        });
+}});
 
 module.exports = router;
